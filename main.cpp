91e13c9f5135b05d02c3f96c21de246c691adfb9
Woocheck <woocheck@op.pl>
2019-12-28 10:23:24 +0100

    main

--- main.cpp
+++ main.cpp
@@ -1,8 +1,20 @@
 #include <iostream>
+#include <math.h>
+#include <complex>
 
+#include "./grid/graph.h"
 
 int main(int argc, char const *argv[])
 {
+    CableLine line( 100, "BU - UW DU", 33000, 1000, 0.04080, 0.170, 0.255, 2*M_1_PI*50*0.255, 100, 0, 5000 );
+
+    std::complex<float> lineZ;
+    std::complex<float> lineY;
+
+    lineZ = line.getZ();
+    lineY = line.getY();
+
+    std::cout << lineZ << std::endl;
 
 
     return 0;
