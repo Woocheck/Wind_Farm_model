02824b4d3327b87a71332f3557ea60c9d7c4c4a0
Woocheck <woocheck@op.pl>
2020-02-03 21:16:08 +0100

    algoritmcpp

---
+++ algorithms/newtonRaphson.cpp
@@ -0,0 +1,26 @@
+#include "./newtonRaphson.h"
+
+void NewtonRaphsonAlgorithm::calculate()
+{
+
+}
+void NewtonRaphsonAlgorithm::imbalanceCalculation()
+{
+
+}
+void NewtonRaphsonAlgorithm::jacobianMatrixCalculation()
+{
+
+}
+void NewtonRaphsonAlgorithm::equationSystemResolve()
+{
+
+}
+void NewtonRaphsonAlgorithm::calculateNewStateVector()
+{
+
+}
+bool NewtonRaphsonAlgorithm::isEpsilonGreater()
+{
+    
+}
