657ed3d92b5754eec44598987a0c7123f43518d9
Woocheck <woocheck@op.pl>
2020-02-03 21:11:38 +0100

    algorithm headet

--- algorithms/newtonRaphson.h
+++ algorithms/newtonRaphson.h
@@ -1,12 +1,28 @@
 #ifndef _NEWTON_RAPHSON_H_
 #define _NEWTON_RAPHSON_H_
 
+#include <complex>
+#include "../grid/grid.h"
 
-class PowerFlow
+class NewtonRaphsonAlgorithm
 {
     private:
-
+        Model _grid;
+        
     public:
+        NewtonRaphsonAlgorithm()
+        {
+            _grid.reloadModel();
+        };
+        void calculate();
+        
+    private:
+        void imbalanceCalculation();
+        void jacobianMatrixCalculation();
+        void equationSystemResolve();
+        void calculateNewStateVector();
+        bool isEpsilonGreater();
+
 };
 
 #endif
