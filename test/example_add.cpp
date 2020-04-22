23eafbe4186895a6a7a35c1c70a5366635988971
Woocheck <woocheck@op.pl>
2020-04-22 21:33:44 +0200

    nev tests

---
+++ test/example_add.cpp
@@ -0,0 +1,9 @@
+#include "gtest/gtest.h"
+#include "../src/grid/node.h"
+
+TEST(example, add)
+{
+    
+    EXPECT_EQ(9,8);
+
+}
---
+++ test/example_subtract.cpp
@@ -0,0 +1,7 @@
+#include "gtest/gtest.h"
+#include "example.h"
+
+TEST(example, subtract)
+{
+    EXPECT_EQ(9,8);
+}
