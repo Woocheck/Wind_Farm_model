35fda033de870e1e60546a5ee1a44d1069fa59e9
Woocheck <woocheck@op.pl>
2020-04-23 17:12:01 +0200

    node and graph

--- test/grid/graph_test.cpp
+++ test/grid/graph_test.cpp
@@ -23,3 +23,30 @@ TEST( Graph_class, get_crossection )
 {
     EXPECT_EQ( 1000, line.getCrossection() );
 }
+TEST( Graph_class, get_Nodes_Numbers )
+{
+    auto nodeNumbes = std::make_pair<int,int>(0,1);
+    EXPECT_EQ( nodeNumbes , line.getNodesNumbers() );
+}
+TEST( Graph_class, get_Conductance )
+{
+    auto conductance = 1 / (std::real( 5 ) + std::imag( 0.5 ));
+    EXPECT_EQ( conductance, line.getConductance() );
+}/*
+TEST( Graph_class, get_I1 )
+{
+    EXPECT_EQ( 1000, line.getCrossection() );
+}
+TEST( Graph_class, get_U1 )
+{
+    EXPECT_EQ( 1000, line.getCrossection() );
+}
+TEST( Graph_class, get_I2 )
+{
+    EXPECT_EQ( 1000, line.getCrossection() );
+}
+TEST( Graph_class, get_U2)
+{
+    EXPECT_EQ( 1000, line.getCrossection() );
+}
+*/
--- test/grid/node_test.cpp
+++ test/grid/node_test.cpp
@@ -4,80 +4,80 @@
 
 Node node( 1, 0, "node_name", 2.0, 3.0, 33000.0, 0.1, 1);
 
-TEST(Node, get_number)
+TEST(Node_class, get_number)
 {
     EXPECT_EQ(1,node.getNumber());
 }
-TEST(Node, get_paent)
+TEST(Node_class, get_paent)
 {
     EXPECT_EQ(0,node.getParentNumber());
 }
-TEST(Node, get_name)
+TEST(Node_class, get_name)
 {
     EXPECT_EQ("node_name",node.getName());
 }
-TEST(Node, get_p)
+TEST(Node_class, get_p)
 {
     EXPECT_EQ(2.0,node.getP());
 }
-TEST(Node, get_q)
+TEST(Node_class, get_q)
 {
     EXPECT_EQ(3.0,node.getQ());
 }
-TEST(Node, get_Module)
+TEST(Node_class, get_Module)
 {
     EXPECT_EQ(33000.0,node.getVoltageModule());
 }
-TEST(Node, get_Argumnt)
+TEST(Node_class, get_Argumnt)
 {
     EXPECT_EQ(0.1,node.getVoltageArgument());
 }
-TEST(Node, get_Category)
+TEST(Node_class, get_Category)
 {
     EXPECT_EQ(1,node.getCategory());
 }
 
-TEST(Node, set_number)
+TEST(Node_class, set_number)
 {
     node.setNumber( 2 );
     EXPECT_EQ(2,node.getNumber());
 }
-TEST(Node, set_paent)
+TEST(Node_class, set_paent)
 {
     node.setParentNumber( 1 );
     EXPECT_EQ(1,node.getParentNumber());
 }
-TEST(Node, set_name)
+TEST(Node_class, set_name)
 {
     node.setName( "new_node_name" );
     EXPECT_EQ( "new_node_name", node.getName() );
 }
-TEST(Node, set_p)
+TEST(Node_class, set_p)
 {
     node.setP( 1.0 );
     EXPECT_EQ( 1.0,node.getP() );
 }
-TEST(Node, set_q)
+TEST(Node_class, set_q)
 {
     node.setQ( 2.0 );
     EXPECT_EQ( 2.0,node.getQ() );
 }
-TEST(Node, set_Module)
+TEST(Node_class, set_Module)
 {
     node.setVoltageModule( 22000.0 );
     EXPECT_EQ( 22000.0, node.getVoltageModule() );
 }
-TEST(Node, set_Argumnt)
+TEST(Node_class, set_Argumnt)
 {
     node.setVoltageArgument( 0.2 );
     EXPECT_EQ( 0.2, node.getVoltageArgument() );
 }
-TEST(Node, set_Category)
+TEST(Node_class, set_Category)
 {
     node.setCategory( 5 );
     EXPECT_EQ( 5, node.getCategory() );
 }
-TEST(Node, get_voltage)
+TEST(Node_class, get_voltage)
 {
     std::complex<double>  result;
     result = std::polar( 22000.0, 0.2 );
