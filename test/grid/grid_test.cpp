f4e7e9e3c86e9eb6e778b1887e1a53c9f2d97031
Woocheck <woocheck@op.pl>
2020-04-29 19:16:14 +0200

    first grid test

--- test/grid/grid_test.cpp
+++ test/grid/grid_test.cpp
@@ -2,3 +2,14 @@
 #include "gtest/gtest.h"
 #include "../src/grid/grid.h"
 
+#include "../src/algorithms/newtonRaphson.h"
+
+InputData data("graphs.txt");
+auto dataPointer { std::make_shared<InputData>( data )};
+Model grid;
+
+TEST( Grid_class, get_number_Reciving )
+{
+    grid.reloadModel();
+    EXPECT_EQ( 1, grid.getNumberReciving() );
+}
