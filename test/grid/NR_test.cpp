ce7390637b28409770d0fcdc0fe688d7762c640f
Woocheck <woocheck@op.pl>
2020-04-30 16:32:21 +0200

    calculate in NR not working

---
+++ test/grid/NR_test.cpp
@@ -0,0 +1,14 @@
+#include <armadillo>
+#include "gtest/gtest.h"
+#include "../src/grid/grid.h"
+
+#include "../src/algorithms/newtonRaphson.h"
+
+TEST( NwetonRaphson_class, calculate )
+{
+    Model grid;
+    grid.reloadModel();
+
+    NewtonRaphsonAlgorithm  newtonRaphsonAlgorithm( grid );
+    //newtonRaphsonAlgorithm.calculate();
+}
