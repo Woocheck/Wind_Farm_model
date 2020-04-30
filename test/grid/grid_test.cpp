05f3284d1c1432ee04f58cc5eb3c1b72824b8f6f
Woocheck <woocheck@op.pl>
2020-04-30 16:10:46 +0200

    changed initialisation

--- test/grid/grid_test.cpp
+++ test/grid/grid_test.cpp
@@ -4,18 +4,22 @@
 
 #include "../src/algorithms/newtonRaphson.h"
 
-InputData data("graphs.txt");
-auto dataPointer { std::make_shared<InputData>( data )};
-Model grid;
+
 
 TEST( Grid_class, get_number_Reciving )
 {
+    InputData data("graphs.txt");
+    auto dataPointer { std::make_shared<InputData>( data )};
+    Model grid;
     grid.reloadModel();
     EXPECT_EQ( 2, grid.getNumberReciving() );
 }
 
 TEST( Grid_class, get_number_Sources )
 {
+    InputData data("graphs.txt");
+    auto dataPointer { std::make_shared<InputData>( data )};
+    Model grid;
     grid.reloadModel();
     EXPECT_EQ( 2, grid.getNumberSource() );
 }
