447cdb17394ef48d0fe095ae81d415f10a3c01d7
Woocheck <woocheck@op.pl>
2020-05-03 16:11:11 +0200

    second cmake

---
+++ src/build/CMakeFiles/main_wind_farm.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/woocheck/Workspace_model/Wind_Farm_model/src/algorithms/newtonRaphson.cpp" "/home/woocheck/Workspace_model/Wind_Farm_model/src/build/CMakeFiles/main_wind_farm.dir/algorithms/newtonRaphson.cpp.o"
+  "/home/woocheck/Workspace_model/Wind_Farm_model/src/file/file_model.cpp" "/home/woocheck/Workspace_model/Wind_Farm_model/src/build/CMakeFiles/main_wind_farm.dir/file/file_model.cpp.o"
+  "/home/woocheck/Workspace_model/Wind_Farm_model/src/grid/graph.cpp" "/home/woocheck/Workspace_model/Wind_Farm_model/src/build/CMakeFiles/main_wind_farm.dir/grid/graph.cpp.o"
+  "/home/woocheck/Workspace_model/Wind_Farm_model/src/grid/grid.cpp" "/home/woocheck/Workspace_model/Wind_Farm_model/src/build/CMakeFiles/main_wind_farm.dir/grid/grid.cpp.o"
+  "/home/woocheck/Workspace_model/Wind_Farm_model/src/grid/node.cpp" "/home/woocheck/Workspace_model/Wind_Farm_model/src/build/CMakeFiles/main_wind_farm.dir/grid/node.cpp.o"
+  "/home/woocheck/Workspace_model/Wind_Farm_model/src/grid/turbine.cpp" "/home/woocheck/Workspace_model/Wind_Farm_model/src/build/CMakeFiles/main_wind_farm.dir/grid/turbine.cpp.o"
+  "/home/woocheck/Workspace_model/Wind_Farm_model/src/main_wind_farm.cpp" "/home/woocheck/Workspace_model/Wind_Farm_model/src/build/CMakeFiles/main_wind_farm.dir/main_wind_farm.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../file"
+  "../grid"
+  "../algorithms"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ src/build/CMakeFiles/main_wind_farm.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/main_wind_farm.dir/algorithms/newtonRaphson.cpp.o"
+  "CMakeFiles/main_wind_farm.dir/file/file_model.cpp.o"
+  "CMakeFiles/main_wind_farm.dir/grid/graph.cpp.o"
+  "CMakeFiles/main_wind_farm.dir/grid/grid.cpp.o"
+  "CMakeFiles/main_wind_farm.dir/grid/node.cpp.o"
+  "CMakeFiles/main_wind_farm.dir/grid/turbine.cpp.o"
+  "CMakeFiles/main_wind_farm.dir/main_wind_farm.cpp.o"
+  "main_wind_farm"
+  "main_wind_farm.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/main_wind_farm.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
